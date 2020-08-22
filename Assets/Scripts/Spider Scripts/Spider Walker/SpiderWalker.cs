9c8918e0a82e3db68817d50cebd72afb615d2e2b
Shajid Hossain <sajidhossainhemal@gmail.com>
2020-08-22 23:55:24 +0600

    Completed Enemy Setup

---
+++ Assets/Scripts/Spider Scripts/Spider Walker/SpiderWalker.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderWalker : MonoBehaviour
+{
+    [SerializeField]
+    private Transform startPos, endPos;
+
+    private bool collision;
+
+    [SerializeField]
+    private float speed = 1f;
+
+    private Rigidbody2D myRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+        ChangeDirection();
+    }
+
+    private void ChangeDirection() {
+        collision = Physics2D.Linecast(startPos.position, endPos.position, 1 << LayerMask.NameToLayer("Ground"));
+        
+        Debug.DrawLine(startPos.position, endPos.position, Color.green);
+
+        Vector2 temp = transform.localScale;
+
+        if(!collision) {
+            temp.x = (-1) * temp.x;
+        }
+
+        transform.localScale = temp;
+    }
+
+    private void Move() {
+        myRigidbody.velocity = new Vector2(transform.localScale.x, 0f) * speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "Player") {
+            Destroy(gameObject);
+        }
+    }
+}
